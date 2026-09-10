e2386144407aa873b13074a9eb3d90ff6703280c
Munachimso Chukwure <munachukwure26@gmail.com>
2026-09-10 09:44:04 +0100

    Clickable usernames and posts

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView
+from django.contrib.auth import login
 
 from .forms import CustomUserCreationForm
 from .models import CustomUser
@@ -10,6 +11,11 @@ class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('home')
     template_name = 'signup.html'
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        login(self.request, self.object)
+        return response
 
 class ProfileView(DetailView):
     model = CustomUser
