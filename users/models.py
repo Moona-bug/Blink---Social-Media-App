216994f1890dff9087f08508406dfe9f70f510af
Munachimso Chukwure <munachukwure26@gmail.com>
2026-09-11 10:40:03 +0100

    Fixed profile background image issue

--- users/models.py
+++ users/models.py
@@ -6,7 +6,7 @@ class CustomUser(AbstractUser):
     age = models.PositiveIntegerField(null=True, blank=True)
     bio = models.TextField(max_length=500, blank=True)
     profile_picture = models.ImageField(
-        upload_to='background_pictures/',
+        upload_to='profile_pictures/',
         blank=True,
         null=True
     )
